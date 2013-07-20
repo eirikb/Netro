44cd854bcadf75b7fd4be1cb33a9412a98777c64
Eirik Brandtzaeg <eirikb@eirikb.no>
2013-07-21 00:26:38 +0200

    Cleanup

--- NetroTest/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NetroTest")]
-[assembly: AssemblyProduct("NetroTest")]
-[assembly: ComVisible(false)]
-[assembly: Guid("0dcfc932-87b4-43d3-82ac-f4aca1512628")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
