1770165b71e4d31bd7436512bc271e89db786f6e
Eirik Brandtzaeg <eirikb@eirikb.no>
2013-07-28 17:40:39 +0200

    Version bump

--- Netro/Properties/AssemblyInfo.cs
+++ Netro/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Netro")]
 [assembly: ComVisible(false)]
 [assembly: Guid("64c033e8-bb86-4924-bd67-85363506b441")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
