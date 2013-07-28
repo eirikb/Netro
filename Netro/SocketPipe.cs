c372e7e4fa7f33c73f5044bd828aeea93b107125
Eirik Brandtzaeg <eirikb@eirikb.no>
2013-07-28 17:39:29 +0200

    Pipe sockets (AsyncSockets for now)

---
+++ Netro/SocketPipe.cs
@@ -0,0 +1,20 @@
+﻿namespace Netro
+{
+    public static class SocketPipe
+    {
+        public static void PipeSockets(AsyncSocket socketA, AsyncSocket socketB)
+        {
+            socketA.Read(socketB.Write);
+            socketB.Read(socketA.Write);
+
+            socketA.Disconnect(socketB.Disconnect);
+            socketB.Disconnect(socketA.Disconnect);
+        }
+
+        public static void PipeReverseSocket(int id, ReverseAsyncSocket reverseSocket, AsyncSocket socket)
+        {
+            reverseSocket.Read((tid, buffer, index, count) => { if (tid == id) socket.Write(buffer, index, count); });
+            socket.Read((buffer, count) => reverseSocket.Write(id, buffer, 0, count));
+        }
+    }
+}
