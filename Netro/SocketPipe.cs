a0dd2169cea5c2122bbeac6c1853ff16e377107c
Eirik Brandtzaeg <eirikb@eirikb.no>
2013-07-30 00:03:26 +0200

    Remove unused method in SocketPipe (For reverse)

--- Netro/SocketPipe.cs
+++ Netro/SocketPipe.cs
@@ -10,11 +10,5 @@
             socketA.Disconnect(socketB.Disconnect);
             socketB.Disconnect(socketA.Disconnect);
         }
-
-        public static void PipeReverseSocket(int id, ReverseAsyncSocket reverseSocket, AsyncSocket socket)
-        {
-            reverseSocket.Read((tid, buffer, index, count) => { if (tid == id) socket.Write(buffer, index, count); });
-            socket.Read((buffer, count) => reverseSocket.Write(id, buffer, 0, count));
-        }
     }
 }
